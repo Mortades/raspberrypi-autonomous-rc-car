fdf719d16f02a8653cf980a912d717c7e3304e47
Mortades <47992281+Mortades@users.noreply.github.com>
2019-06-20 19:42:32 +0100

    Added WideLeftTurn
    Works
    A = Right Motor
    B =Left Motor

---
+++ Mod4-driving.py
@@ -0,0 +1,94 @@
+# CamJam EduKit 3 - Robotics
+# Worksheet 4 - Driving and Turning
+# A = Right Motor
+# B = Left Motor
+import RPi.GPIO as GPIO  # Import the GPIO Library
+import time  # Import the Time library
+
+# Set the GPIO modes
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+# Set variables for the GPIO motor pins
+pinMotorAForwards = 10
+pinMotorABackwards = 9
+pinMotorBForwards = 8
+pinMotorBBackwards = 7
+
+# Set the GPIO Pin mode
+GPIO.setup(pinMotorAForwards, GPIO.OUT)
+GPIO.setup(pinMotorABackwards, GPIO.OUT)
+GPIO.setup(pinMotorBForwards, GPIO.OUT)
+GPIO.setup(pinMotorBBackwards, GPIO.OUT)
+
+
+# Turn all motors off
+def stopmotors():
+    GPIO.output(pinMotorAForwards, 0)
+    GPIO.output(pinMotorABackwards, 0)
+    GPIO.output(pinMotorBForwards, 0)
+    GPIO.output(pinMotorBBackwards, 0)
+
+
+# Turn both motors forwards
+def forwards():
+    GPIO.output(pinMotorAForwards, 1)
+    GPIO.output(pinMotorABackwards, 0)
+    GPIO.output(pinMotorBForwards, 1)
+    GPIO.output(pinMotorBBackwards, 0)
+
+
+# Turn both motors backwards
+def backwards():
+    GPIO.output(pinMotorAForwards, 0)
+    GPIO.output(pinMotorABackwards, 1)
+    GPIO.output(pinMotorBForwards, 0)
+    GPIO.output(pinMotorBBackwards, 1)
+
+
+# Turn left
+def left():
+    GPIO.output(pinMotorAForwards, 0)
+    GPIO.output(pinMotorABackwards, 1)
+    GPIO.output(pinMotorBForwards, 1)
+    GPIO.output(pinMotorBBackwards, 0)
+# Homebrew WideTurnLeft
+def WideRightTurn(duration):
+    GPIO.output(pinMotorABackwards, 0)
+    GPIO.output(pinMotorBBackwards, 0)
+    GPIO.output(pinMotorBForwards, 1)
+# Loop for 1 second per each duration turning A on and off to replicate half speed
+    for i in range(duration):
+         GPIO.output(pinMotorAForwards, 1)
+         time.sleep(0.5)
+         GPIO.output(pinMotorAForwards, 0)
+         time.sleep(0.5)
+
+# Turn Right
+def right():
+    GPIO.output(pinMotorAForwards, 1)
+    GPIO.output(pinMotorABackwards, 0)
+    GPIO.output(pinMotorBForwards, 0)
+    GPIO.output(pinMotorBBackwards, 1)
+
+WideLeftTurn(5)
+
+
+#forwards()
+#time.sleep(1)  # Pause for 1 second
+
+#left()
+#time.sleep(0.5)  # Pause for half a second
+
+#forwards()
+#time.sleep(1)
+
+#right()
+#time.sleep(0.5)
+
+#backwards()
+#time.sleep(0.5)
+
+stopmotors()
+
+GPIO.cleanup()
